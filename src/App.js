20d0abf6ab95418f54b98dadab0f53e2e37ed792
Rowee Apor <75659500+Rowee13@users.noreply.github.com>
2022-10-31 23:41:26 +0800

    Added color helper to generate color format
    used chroma-js as dependency

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Component } from "react";
 
 import Palette from "./components/Palette";
-// import SeedColors from "./SeedColors";
-import "./App.css";
 import SeedColors from "./SeedColors";
+import { generatePalette } from "./helpers/colorHeloer";
+import "./App.css";
 
 class App extends Component {
 	render() {
+		console.log(generatePalette(SeedColors[4]));
 		return (
 			<div>
 				<Palette {...SeedColors[4]} />
