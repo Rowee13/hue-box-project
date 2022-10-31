9e2ce32d4569dfc18fde6810f21d16d49b031881
Rowee Apor <75659500+Rowee13@users.noreply.github.com>
2022-10-31 21:35:08 +0800

    Added Palette component and inital data source

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Palette from "./components/Palette";
+// import SeedColors from "./SeedColors";
+import "./App.css";
+import SeedColors from "./SeedColors";
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Palette {...SeedColors[4]} />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/Palette.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Palette extends Component {
+	render() {
+		return (
+			<div className="Palette">
+				{/* NavBar goes here */}
+				<div className="Palette-colors">{/* color boxes here */}</div>
+			</div>
+		);
+	}
+}
+
+export default Palette;
