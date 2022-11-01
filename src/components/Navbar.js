11800fca500710b6aaba79d5879291eed569fad6
Rowee Apor <75659500+Rowee13@users.noreply.github.com>
2022-11-01 15:49:14 +0800

    Added color format select using material UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,28 @@
 import { Component } from "react";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
 
 import "./Navbar.css";
 
 class Navbar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { format: "hex" };
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(e) {
+		this.setState({ format: e.target.value });
+		this.props.handleChange(e.target.value);
+	}
+
 	render() {
 		const { level, changeLevel } = this.props;
+		const { format } = this.state;
 
 		return (
 			<header className="Navbar">
@@ -24,6 +40,22 @@ class Navbar extends Component {
 							onAfterChange={changeLevel}
 						/>
 					</div>
+				</div>
+				<div className="select-container">
+					<FormControl variant="standard">
+						<InputLabel id="demo-simple-select-standard-label">
+							Color Format
+						</InputLabel>
+						<Select value={format} onChange={this.handleChange}>
+							<MenuItem value="hex">HEX - #ffffff</MenuItem>
+							<MenuItem value="rgb">
+								RGB - rgb(255, 255, 255)
+							</MenuItem>
+							<MenuItem value="rgba">
+								RGBA - rgb(255, 255, 255, 1.0)
+							</MenuItem>
+						</Select>
+					</FormControl>
 				</div>
 			</header>
 		);
